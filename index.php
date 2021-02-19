120ad244cca708903100d72c6f8bf850da3c88c8
William Torres <william@sisoft.com.co>
2021-02-19 15:22:53 -0500

    Suprimo Infortmacion que no aplica para este proyeto

--- index.php
+++ index.php
@@ -15,5 +15,6 @@ $dotenv->load();
 $router = new Suplos\Core\Enrutador();
 
 $router->agregar('', ['controller' => 'HomeController', 'action' => 'index']);
+$router->agregar('data/index', ['controller' => 'DataController', 'action' => 'index']);
 
 $router->dispatch($_SERVER['QUERY_STRING']);
--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 /*
   Creación de una función personalizada para jQuery que detecta cuando se detiene el scroll en la página
 */
-$.fn.scrollEnd = function(callback, timeout) {
-  $(this).scroll(function(){
-    var $this = $(this);
-    if ($this.data('scrollTimeout')) {
-      clearTimeout($this.data('scrollTimeout'));
-    }
-    $this.data('scrollTimeout', setTimeout(callback,timeout));
-  });
-};
+// $.fn.scrollEnd = function(callback, timeout) {
+//   $(this).scroll(function(){
+//     var $this = $(this);
+//     if ($this.data('scrollTimeout')) {
+//       clearTimeout($this.data('scrollTimeout'));
+//     }
+//     $this.data('scrollTimeout', setTimeout(callback,timeout));
+//   });
+// };
 /*
   Función que inicializa el elemento Slider
 */
@@ -49,4 +49,4 @@ function playVideoOnScroll(){
 }
 
 inicializarSlider();
-playVideoOnScroll();
+//playVideoOnScroll();
